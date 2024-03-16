3665b382539001458adc3d6d4f25b477619663f1
ig-imanish <manishsailakhurd83@gmail.com>
2024-03-16 11:06:32 +0530

    Project Done

--- src/main/java/com/urlshortner/shortxurl/ShortxurlApplication.java
+++ src/main/java/com/urlshortner/shortxurl/ShortxurlApplication.java
@@ -2,7 +2,9 @@ package com.urlshortner.shortxurl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ShortxurlApplication {
 
--- src/main/java/com/urlshortner/shortxurl/controller/MainController.java
+++ src/main/java/com/urlshortner/shortxurl/controller/MainController.java
@@ -1,16 +1,18 @@
 package com.urlshortner.shortxurl.controller;
 
+import java.net.URI;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,79 +20,245 @@ import com.urlshortner.shortxurl.Model.Url;
 import com.urlshortner.shortxurl.helpers.Helper;
 import com.urlshortner.shortxurl.service.UrlService;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class MainController {
 
+    private final int limit = 50;
+
     @Value("${your.domain.name}")
     String domain;
+
     @Autowired
     UrlService urlService;
 
     @Autowired
     Helper helper;
 
-    @Autowired
-    Url url;
-
     @GetMapping("/")
     public String showHomePage() {
-        return "index";
+        return "redirect:/index";
     }
 
     @GetMapping("/index")
-    public String showIndexPage() {
+    public String showIndexPage(HttpSession session, Model model) {
+        Integer count = (Integer) session.getAttribute("count");
+        if (count == null) {
+            count = 0;
+        }
+        System.out.println(count);
+        model.addAttribute("count", count);
+        model.addAttribute("limit", limit);
         return "index";
     }
 
     @PostMapping("/shortxurl")
     public String shortUrlRedirect(@RequestParam("originalUrl") String originalUrl,
             @RequestParam("customName") String customName,
-            RedirectAttributes redirectAttributes) {
-        // Check if the original URL and custom name are not null and not empty
+            RedirectAttributes redirectAttributes, HttpSession session, Model model) {
         if (originalUrl == null || originalUrl.isEmpty() || customName == null || customName.isEmpty()) {
             redirectAttributes.addFlashAttribute("urlNotValid", "URL and custom name cannot be empty");
             return "redirect:/index";
         }
 
-        // Check if the original URL matches the valid URL pattern
         String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(originalUrl);
 
         if (!matcher.matches()) {
-            // If the URL does not match the pattern, handle the invalid URL case
             redirectAttributes.addFlashAttribute("urlNotValid", "Provided URL is not valid");
             return "redirect:/index";
         }
 
-        // Check if the original URL or custom name exceeds certain lengths
         if (originalUrl.length() > 2048 || customName.length() > 50) {
             redirectAttributes.addFlashAttribute("urlNotValid", "URL or custom name is too long");
             return "redirect:/index";
         }
 
-        // Proceed with URL shortening process
+        Integer count = (Integer) session.getAttribute("count");
+        if (count == null) {
+            count = 0;
+        }
+
+        if (count >= limit) {
+            model.addAttribute("limit", limit);
+            model.addAttribute("count", count);
+            return "redirect:/index";
+        }
+
+        session.setAttribute("count", ++count);
+
+        Url url = new Url();
         url.setCustomName(customName);
         url.setOriginalUrl(originalUrl);
-        String shorterUrl = helper.generateRandomString(url.getCustomName());
+        String shorterUrl = helper.generateRandomString(customName);
         url.setShortUrl(shorterUrl);
-
-        System.out.println(shorterUrl);
 
         Url savedUrl = urlService.saveUrl(url);
         redirectAttributes.addFlashAttribute("domain", domain);
         redirectAttributes.addFlashAttribute("shortenedUrl", savedUrl.getShortUrl());
-
         return "redirect:/index";
     }
 
+    @SuppressWarnings("null")
     @GetMapping("/{shortUrl}")
-    public String redirectToOrgUrl(@PathVariable("shortUrl") String shortUrl) {
+    public ResponseEntity<?> redirectToOrgUrl(@PathVariable("shortUrl") String shortUrl) {
         Url originalUrl = urlService.getUrlByUrl(shortUrl);
-        if (originalUrl != null) {
-            return "redirect:" + originalUrl.getOriginalUrl();
+        if (originalUrl != null && originalUrl.getOriginalUrl() != null) {
+            return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(originalUrl.getOriginalUrl())).build();
         } else {
-            return "index";
+            return ResponseEntity.notFound().build();
         }
     }
+
 }
+
+/*
+ * package com.urlshortner.shortxurl.controller;
+ * 
+ * import java.net.URI;
+ * import java.util.regex.Matcher;
+ * import java.util.regex.Pattern;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.beans.factory.annotation.Value;
+ * import org.springframework.http.HttpStatus;
+ * import org.springframework.http.ResponseEntity;
+ * import org.springframework.scheduling.annotation.Scheduled;
+ * import org.springframework.stereotype.Controller;
+ * import org.springframework.ui.Model;
+ * import org.springframework.web.bind.annotation.GetMapping;
+ * import org.springframework.web.bind.annotation.PathVariable;
+ * import org.springframework.web.bind.annotation.PostMapping;
+ * import org.springframework.web.bind.annotation.RequestParam;
+ * import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+ * 
+ * import com.urlshortner.shortxurl.Model.Url;
+ * import com.urlshortner.shortxurl.helpers.Helper;
+ * import com.urlshortner.shortxurl.service.UrlService;
+ * 
+ * import jakarta.servlet.http.HttpSession;
+ * 
+ * @Controller
+ * public class MainController {
+ * 
+ * private final int limit = 2;
+ * private int countx = 0;
+ * Integer count = countx;
+ * 
+ * @Value("${your.domain.name}")
+ * String domain;
+ * 
+ * @Autowired
+ * UrlService urlService;
+ * 
+ * @Autowired
+ * Helper helper;
+ * 
+ * @Autowired
+ * Url url;
+ * 
+ * @Scheduled(fixedDelay = 60000) // 60 seconds = 1 minute
+ * public void resetCount(HttpSession session, Model model) {
+ * countx = 0;
+ * count = (Integer) session.getAttribute("count");
+ * session.setAttribute("count", countx);
+ * session.setAttribute("countx", countx);
+ * 
+ * model.addAttribute("count", count);
+ * }
+ * 
+ * @GetMapping("/")
+ * public String showHomePage() {
+ * 
+ * return "redirect:/index";
+ * }
+ * 
+ * @GetMapping("/index")
+ * public String showIndexPage(HttpSession session, Model model) {
+ * count = (Integer) session.getAttribute("countx");
+ * if (count == null) {
+ * count = 0;
+ * }
+ * System.out.println("countx" + countx);
+ * System.out.println("count" + count);
+ * System.out.println("limit" + limit);
+ * model.addAttribute("count", count);
+ * 
+ * model.addAttribute("limit", limit);
+ * 
+ * return "index";
+ * }
+ * 
+ * @PostMapping("/shortxurl")
+ * public String shortUrlRedirect(@RequestParam("originalUrl") String
+ * originalUrl,
+ * 
+ * @RequestParam("customName") String customName,
+ * RedirectAttributes redirectAttributes, HttpSession session, Model model) {
+ * if (originalUrl == null || originalUrl.isEmpty() || customName == null ||
+ * customName.isEmpty()) {
+ * redirectAttributes.addFlashAttribute("urlNotValid",
+ * "URL and custom name cannot be empty");
+ * return "redirect:/index";
+ * }
+ * 
+ * String regex =
+ * "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+ * Pattern pattern = Pattern.compile(regex);
+ * Matcher matcher = pattern.matcher(originalUrl);
+ * 
+ * if (!matcher.matches()) {
+ * redirectAttributes.addFlashAttribute("urlNotValid",
+ * "Provided URL is not valid");
+ * return "redirect:/index";
+ * }
+ * 
+ * if (originalUrl.length() > 2048 || customName.length() > 50) {
+ * redirectAttributes.addFlashAttribute("urlNotValid",
+ * "URL or custom name is too long");
+ * return "redirect:/index";
+ * }
+ * 
+ * count = (Integer) session.getAttribute("xcount");
+ * 
+ * System.out.println(count);
+ * if (count == null) {
+ * count = 0;
+ * }
+ * 
+ * if (count >= limit) {
+ * model.addAttribute("limit", limit);
+ * model.addAttribute("count", count);
+ * return "redirect:/index";
+ * }
+ * 
+ * session.setAttribute("countx", countx++);
+ * 
+ * url.setCustomName(customName);
+ * url.setOriginalUrl(originalUrl);
+ * String shorterUrl = helper.generateRandomString(url.getCustomName());
+ * url.setShortUrl(shorterUrl);
+ * 
+ * System.out.println(shorterUrl);
+ * Url savedUrl = urlService.saveUrl(url);
+ * redirectAttributes.addFlashAttribute("domain", domain);
+ * redirectAttributes.addFlashAttribute("shortenedUrl", savedUrl.getShortUrl());
+ * return "redirect:/index";
+ * }
+ * 
+ * @GetMapping("/{shortUrl}")
+ * public ResponseEntity<?> redirectToOrgUrl(@PathVariable("shortUrl") String
+ * shortUrl) {
+ * Url originalUrl = urlService.getUrlByUrl(shortUrl);
+ * if (originalUrl != null) {
+ * return
+ * ResponseEntity.status(HttpStatus.FOUND).location(URI.create(originalUrl.
+ * getOriginalUrl())).build();
+ * } else {
+ * return ResponseEntity.notFound().build();
+ * }
+ * }
+ * }
+ */
