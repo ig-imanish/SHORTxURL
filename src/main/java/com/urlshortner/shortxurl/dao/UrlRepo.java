cbdee0dd5c3af95f8fffa6fabf29beb5b96c28e4
Manish Kumar <153966717+ig-imanish@users.noreply.github.com>
2024-07-02 12:26:17 +0530

    Update UrlRepo.java

--- src/main/java/com/urlshortner/shortxurl/dao/UrlRepo.java
+++ src/main/java/com/urlshortner/shortxurl/dao/UrlRepo.java
@@ -1,10 +1,12 @@
 package com.urlshortner.shortxurl.dao;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
 import com.urlshortner.shortxurl.Model.Url;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UrlRepo extends CrudRepository<Url, Integer>{
+public interface UrlRepo extends MongoRepository<Url, Integer>{
     public Url findByShortUrl(String url);
 }
