251af97174c79fb52bb7f478c99c830d02b520d2
Manish Kumar <153966717+ig-imanish@users.noreply.github.com>
2024-07-03 07:15:25 +0530

    Create ExceptionController.java

---
+++ src/main/java/com/urlshortner/shortxurl/controller/ExceptionController.java
@@ -0,0 +1,27 @@
+package com.urlshortner.shortxurl.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Object> handleMethodNotSupported(
+            HttpRequestMethodNotSupportedException ex, WebRequest request) {
+        String message = "Request method '" + ex.getMethod() + "' is not supported";
+        return new ResponseEntity<>(message, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    // Add more exception handlers for other exceptions as needed
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        String message = "An error occurred";
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
