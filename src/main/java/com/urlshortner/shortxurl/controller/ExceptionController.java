44dd844886fc9d7eb64dbe3dfcd196d582c688fb
Manish Kumar <153966717+ig-imanish@users.noreply.github.com>
2024-07-03 07:25:58 +0530

    Update ExceptionController.java

--- src/main/java/com/urlshortner/shortxurl/controller/ExceptionController.java
+++ src/main/java/com/urlshortner/shortxurl/controller/ExceptionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class ExceptionController {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<Object> handleMethodNotSupported(
